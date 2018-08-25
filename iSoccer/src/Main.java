6c294d8e8f8b3f49e9b338624663edd488eb311a
Rafael Monteiro Laranjeira <rml@ic.ufal.br>
2018-08-25 06:40:09 -0300

    Delete Main.java

--- iSoccer/src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    static boolean var = false;
-    public static void main(String[] args)
-    {
-        Menu menu = new Menu();
-        while(!var) Menu.displayMenu();
-    }
-}
